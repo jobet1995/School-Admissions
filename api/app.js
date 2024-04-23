5ab85cd0779e87df4e403b427d7e990852693f1d
Jobet Casquejo <151836012+jobet9530@users.noreply.github.com>
2024-04-23 14:55:04 +0000

    style: Refactor code formatting  file

--- api/app.js
+++ api/app.js
@@ -1,14 +1,14 @@
-const express = require('express')
-const cors = require('cors')
-const authen = require('./middleware/authen')
-const router = require('./router.js')
-const util = require('./util')
+const express = require("express");
+const cors = require("cors");
+const authen = require("./middleware/authen");
+const router = require("./router.js");
+const util = require("./util");
 
-const app = express()
-app.use(cors(), express.json())
-app.use('/uploads', express.static('uploads'))
-app.use('/api', authen, router)
-app.use((err, req, res, next) => {
-  res.status(500).send(util.getError(err))
-})
-app.listen(8000)
+const app = express();
+app.use(cors(), express.json());
+app.use("/uploads", express.static("uploads"));
+app.use("/api", authen, router);
+app.use((err, _req, res, _next) => {
+  res.status(500).send(util.getError(err));
+});
+app.listen(8000);
